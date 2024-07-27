8b80c9755ec6e9292ee8ea3d06f5e474868ae441
ArmenioHouane <166590270+ArmenioHouane@users.noreply.github.com>
2024-07-27 10:49:40 +0200

    fix attempt

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,11 @@
 "use client";
 import ThemeToggle from "./toggle";
 import { Button } from "./material";
+import { FaGithub } from "react-icons/fa";
+import { IconButton } from "./material";
+
+
+
 
 const Hero = () => {
   return (
@@ -29,6 +34,14 @@ const Hero = () => {
 
               <Button>Button</Button>
 
+              <div className="flex gap-4">
+       <a href="https://web.facebook.com/armenio.amalio.5" target="_blank" rel="noopener noreferrer">
+              <IconButton variant="text" color="gray" >
+                <FaGithub className="text-lg  dark:text-white hover:text-blue-500 dark:hover:text-blue-500" />
+               
+              </IconButton>
+            </a>
+    </div>
 
              
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 
 
 import Image from "next/image";
-import ThemeToggle from "./components/toggle";
+
 import Hero from "./components/hero";
 import Cardal from "./components/card";
-import Icon from "./components/icon";
 
 export default function Home() {
   return (
     <>
     <Hero />
-    <Icon />
+
     <Cardal />
 
 
