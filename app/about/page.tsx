e9b9b568d0a0d2305d4b58d4e474ecf494faf36e
ArmenioHouane <166590270+ArmenioHouane@users.noreply.github.com>
2024-07-28 17:50:53 +0200

    last

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -327,7 +327,7 @@ const AboutPage = () => {
             </div>
             <Image
               alt="Product screenshot"
-              src="/images/img.jpg"
+              src="/images/img.JPG"
               width={2432}
               height={1442}
               className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
